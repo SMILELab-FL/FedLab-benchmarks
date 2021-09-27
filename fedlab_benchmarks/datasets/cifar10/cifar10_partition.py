3160f0ebf33b1dd04d85315d1a390f075ad73578
AgentDS <zszxlsq@gmail.com>
2021-09-27 10:34:59 +0800

    add cifar10, cifar100 data partition examples

--- fedlab_benchmarks/datasets/cifar10/cifar10_partition.py
+++ fedlab_benchmarks/datasets/cifar10/cifar10_partition.py
@@ -1,15 +1,21 @@
-import sys
+# import sys
 
 
-from fedlab.utils.functional import save_dict
-from fedlab.utils.dataset.slicing import noniid_slicing, random_slicing
+# from fedlab.utils.functional import save_dict
+# from fedlab.utils.dataset.slicing import noniid_slicing, random_slicing
 
-import torchvision
+# import torchvision
 
-trainset = torchvision.datasets.CIFAR10(root="./", train=True, download=False)
+# trainset = torchvision.datasets.CIFAR10(root="./", train=True, download=False)
 
-data_indices = noniid_slicing(trainset, num_clients=100, num_shards=200)
-save_dict(data_indices, "cifar10_noniid.pkl")
+# data_indices = noniid_slicing(trainset, num_clients=100, num_shards=200)
+# save_dict(data_indices, "cifar10_noniid.pkl")
 
-data_indices = random_slicing(trainset, num_clients=100)
-save_dict(data_indices, "cifar10_iid.pkl")
+# data_indices = random_slicing(trainset, num_clients=100)
+# save_dict(data_indices, "cifar10_iid.pkl")
+
+"""
+Please refer to cifar10_partition.ipynb file for usage of CIFAR10Partitioner.
+
+Function ``random_slicing()`` and ``noniid_slicing()`` are deprecated in the future version.
+"""
