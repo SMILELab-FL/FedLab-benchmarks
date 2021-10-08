93c92588afac1ebd76b334b730428e22ca7318b9
AgentDS <zszxlsq@gmail.com>
2021-10-08 22:15:37 +0800

    ..

--- fedlab_benchmarks/feature-skew-fedavg/data_partition.py
+++ fedlab_benchmarks/feature-skew-fedavg/data_partition.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=2021)
     args = parser.parse_args()
 
-    root = "../../../../data/FMNIST"
+    root = "../../../../datasets/FMNIST"
     trainset = FashionMNIST(root=root, train=True, download=True)
 
     # perform partition
