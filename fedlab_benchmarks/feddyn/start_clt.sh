b7fbca693a341762801d3a43d84f8898cf049306
AgentDS <zszxlsq@gmail.com>
2021-10-14 17:05:42 +0800

    ..

--- fedlab_benchmarks/feddyn/start_clt.sh
+++ fedlab_benchmarks/feddyn/start_clt.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # balance iid cifar10 for 100 clients, check config.py for other setting
 python data_partition.py --partition iid --balance True --dataset cifar10 --num-clients 100
+
+# launch client server
+# TODO
+
