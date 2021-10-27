000c76ff3eb1ddad5bf69055a501aff9a06c4949
zengdun <928255708@qq.com>
2021-10-27 10:41:19 +0800

    move FedLab compressors to here

--- fedlab_benchmarks/compressors/dgc.py
+++ fedlab_benchmarks/compressors/dgc.py
@@ -1,14 +1,87 @@
-"""
-codes are modified from https://github.com/synxlin/deep-gradient-compression
-"""
+# Copyright 2021 Peng Cheng Laboratory (http://www.szpclab.com/) and FedLab Authors (smilelab.group)
 
-import math
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
+import math
 import random
-from .memory import Memory
+from .base_compressor import Memory, Compressor
 
 
-class DGCCompressor:
+class DGCSGDMemory(Memory):
+    """ Memory for momentum correction in DGC for momentum SGD optimizer"""
+    def __init__(self,
+                 momentum=0.9,
+                 nesterov=False,
+                 gradient_clipping=None,
+                 momentum_masking=True):
+        self.gradient_clipping = gradient_clipping
+        self.momentum_masking = momentum_masking
+
+        self.momentum = momentum
+        self.nesterov = nesterov
+        self.momentums = {}
+        self.velocities = {}
+
+    def initialize(self, named_parameters):
+        for name, param in named_parameters:
+            self.momentums[name] = torch.zeros_like(param.data)
+            self.velocities[name] = torch.zeros_like(param.data)
+
+    def compensate(self, grad, name, accumulate=True):
+        """Update the velocities with the momentums."""
+        if self.gradient_clipping is not None:
+            grad = self.gradient_clipping(grad)
+        mmt = self.momentums[name]
+        if accumulate:
+            vec = self.velocities[name]
+            if self.nesterov:
+                mmt.add_(grad).mul_(self.momentum)
+                vec.add_(mmt).add_(grad)
+            else:
+                mmt.mul_(self.momentum).add_(grad)
+                vec.add_(mmt)
+            return vec
+        else:
+            if self.nesterov:
+                mmt.add_(grad).mul_(self.momentum)
+                return mmt.add(grad)
+            else:
+                mmt.mul_(self.momentum).add_(grad)
+                return mmt.clone()  # TODO: save this clone
+
+    def update(self, name, ctx):
+        """Update the momentums."""
+        indices = ctx[0]
+        if self.momentum_masking:
+            self.momentums[name].view(-1).index_fill_(
+                0, indices, 0
+            )  # index_fill_(dim,index,val)按照参数index总的索引数确定的顺序，将原tensor用参数val值填充
+        self.velocities[name].view(-1).index_fill_(0, indices, 0)
+
+    def state_dict(self):
+        return dict(momentums=self.momentums, velocities=self.velocities)
+
+    def load_state_dict(self, state_dict):
+        momentums = state_dict['momentums']
+        velocities = state_dict['velocities']
+        for name in self.momentums.keys():
+            if name in momentums:
+                self.momentums[name] = momentums[name]
+                self.velocities[name] = velocities[name]
+
+
+class DGCCompressor(Compressor):
     def __init__(self,
                  compress_ratio,
                  memory=None,
@@ -55,10 +128,7 @@ class DGCCompressor:
         self.attributes = {}
 
     def initialize(self, named_parameters):
-        """
-        if hvd.rank() == 0:
-            print("=> initializing dgc compressor")
-        """
+
         for name, param in named_parameters:
             if torch.is_tensor(param):
                 numel = param.numel()
