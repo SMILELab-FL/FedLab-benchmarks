4260f82402a2c0910d8511c490caafb4040a7a43
AgentDS <zszxlsq@gmail.com>
2021-11-07 23:16:09 +0800

    add debug command

--- fedlab_benchmarks/feddyn/start_clt.sh
+++ fedlab_benchmarks/feddyn/start_clt.sh
@@ -2,14 +2,14 @@
 # ==========================================
 # =============== EXPERIMENT ===============
 # ==========================================
-ClientRankNum=10
-ClientNumPerRank=10
-ClientNum=$(($ClientNumPerRank * $ClientRankNum))
-WorldSize=$(($ClientRankNum + 1))
-# balance iid cifar10 for 100 clients, check config.py for other setting
-python data_partition.py --out-dir ./Output/run1 --partition iid --balance True --dataset cifar10 --num-clients ${ClientNum} --seed 0
-echo -e "Data partition DONE.\n\n"
-sleep 4s
+#ClientRankNum=10
+#ClientNumPerRank=10
+#ClientNum=$(($ClientNumPerRank * $ClientRankNum))
+#WorldSize=$(($ClientRankNum + 1))
+## balance iid cifar10 for 100 clients, check config.py for other setting
+#python data_partition.py --out-dir ./Output/run1 --partition iid --balance True --dataset cifar10 --num-clients ${ClientNum} --seed 0
+#echo -e "Data partition DONE.\n\n"
+#sleep 4s
 
 # launch client server
 # ------ FedAvg
@@ -24,15 +24,15 @@ sleep 4s
 # wait
 
 # ------ FedDyn
-for ((i = 1; i <= ${ClientRankNum}; i++)); do
-  {
-    echo "client ${i} started"
-    python client_starter.py --world_size ${WorldSize} --rank ${i} --client-num-per-rank ${ClientNumPerRank} --alg FedDyn --out-dir ./Output/FedDyn/run1 &
-    sleep 2s
-  }
-done
-
-wait
+#for ((i = 1; i <= ${ClientRankNum}; i++)); do
+#  {
+#    echo "client ${i} started"
+#    python client_starter.py --world_size ${WorldSize} --rank ${i} --client-num-per-rank ${ClientNumPerRank} --alg FedDyn --out-dir ./Output/FedDyn/run1 &
+#    sleep 2s
+#  }
+#done
+#
+#wait
 
 # ===========================================
 # ================== DEBUG ==================
@@ -41,16 +41,12 @@ ClientRankNum=3
 ClientNumPerRank=10
 ClientNum=$(($ClientNumPerRank * $ClientRankNum))
 WorldSize=$(($ClientRankNum + 1))
-# balance iid cifar10 for 100 clients, check config.py for other setting
-python data_partition.py --out-dir ./Output/debug --partition iid --balance True --dataset cifar10 --num-clients ${ClientNum} --seed 0
-echo -e "Data partition DONE.\n\n"
-sleep 4s
 
 # ------ FedDyn
 for ((i = 1; i <= ${ClientRankNum}; i++)); do
   {
     echo "client ${i} started"
-    python client_starter.py --world_size ${WorldSize} --rank ${i} --client-num-per-rank ${ClientNumPerRank} --alg FedDyn --out-dir ./Output/debug &
+    python client_starter.py --debug --world_size ${WorldSize} --rank ${i} --client-num-per-rank ${ClientNumPerRank} --alg FedDyn --out-dir ./Output/FedDyn/debug/ &
     sleep 2s
   }
 done
--- fedlab_benchmarks/feddyn/start_server.sh
+++ fedlab_benchmarks/feddyn/start_server.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-ClientRankNum=10
-ClientNumPerRank=10
-ClientNum=$(($ClientNumPerRank * $ClientRankNum))
-WorldSize=$(($ClientRankNum + 1))
+# ==========================================
+# =============== EXPERIMENT ===============
+# ==========================================
+#ClientRankNum=10
+#ClientNumPerRank=10
+#ClientNum=$(($ClientNumPerRank * $ClientRankNum))
+#WorldSize=$(($ClientRankNum + 1))
 # balance iid cifar10 for 100 clients, check config.py for other setting
 # python data_partition.py --out-dir ./Output/ --partition iid --balance True --dataset cifar10 --num-clients ${ClientNum} --seed 0
 # echo -e "Data partition DONE.\n\n"
 # sleep 4s
 
-# # ====== FedAvg
+# # ----- FedAvg
 # SECONDS=0
 
 # python server_starter.py --world_size 11 --partition iid --alg FedAvg --out-dir ./Output/FedAvg/run1
@@ -16,11 +19,33 @@ WorldSize=$(($ClientRankNum + 1))
 # ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 # echo ELAPSED
 
+# ------- FedDyn
+#SECONDS=0
+#
+#python server_starter.py --world_size ${WorldSize} --partition iid --alg FedDyn --out-dir ./Output/FedDyn/run1
+#
+#ELAPSED="Elapsed: $(($SECONDS / 60))min $(($SECONDS % 60))sec"
+#echo $ELAPSED
 
-# ====== FedDyn
+
+
+# ===========================================
+# ================== DEBUG ==================
+# ===========================================
+ClientRankNum=3
+ClientNumPerRank=10
+ClientNum=$(($ClientNumPerRank * $ClientRankNum))
+WorldSize=$(($ClientRankNum + 1))
+
+# balance iid cifar10 for 30 clients, check config.py for other setting
+python data_partition.py --out-dir ./Output/FedDyn/debug/ --partition iid --balance True --dataset cifar10 --num-clients ${ClientNum} --seed 0
+echo -e "Data partition DONE.\n\n"
+sleep 4s
+
+# ------- FedDyn
 SECONDS=0
 
-python server_starter.py --world_size 11 --partition iid --alg FedDyn --out-dir ./Output/FedDyn/run1
+python server_starter.py --debug --world_size ${WorldSize} --partition iid --alg FedDyn --out-dir ./Output/FedDyn/debug/
 
 ELAPSED="Elapsed: $(($SECONDS / 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
