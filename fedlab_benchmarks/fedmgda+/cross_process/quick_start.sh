01d35b84a8df0839f1979fefa9e5983ca637ff0a
zed <928255708@qq.com>
2022-04-05 19:45:06 +0800

    [update] v1.0 fedmdga+

---
+++ fedlab_benchmarks/fedmgda+/cross_process/quick_start.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+python server.py --ip 127.0.0.1 --port 3002 --world_size 3 --dataset mnist --round 1 &
+
+python client.py --ip 127.0.0.1 --port 3002 --world_size 3 --rank 1 --dataset mnist &
+
+python client.py --ip 127.0.0.1 --port 3002 --world_size 3 --rank 2 --dataset mnist &
+
+wait
