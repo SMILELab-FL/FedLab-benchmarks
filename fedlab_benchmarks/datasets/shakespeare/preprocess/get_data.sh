c03d0cd0ca08d809ca871e44db91970d5e5bb664
Starry-Hu <starryhu@foxmail.com>
2022-05-13 21:21:42 +0800

    optimize leaf shell: fine-tune structure, keep the raw zip data, and add some hints

--- fedlab_benchmarks/datasets/shakespeare/preprocess/get_data.sh
+++ fedlab_benchmarks/datasets/shakespeare/preprocess/get_data.sh
@@ -2,9 +2,15 @@
 
 cd ../data/raw_data
 
-wget http://www.gutenberg.org/files/100/old/1994-01-100.zip
+if [ ! -f 1994-01-100.zip ]; then
+  wget http://www.gutenberg.org/files/100/old/1994-01-100.zip
+else
+  echo "using existing 1994-01-100.zip"
+fi
+
+echo "unzipping 1994-01-100.zip"
 unzip 1994-01-100.zip
-rm 1994-01-100.zip
+#rm 1994-01-100.zip
 mv 100.txt raw_data.txt
 
 #wget --adjust-extension http://www.gutenberg.org/files/100/100-0.txt
